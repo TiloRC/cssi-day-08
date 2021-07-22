af11acd9161f15e1b1f0d5783ec4a398a647c547
TiloRC <tiloreneau@gmail.com>
2021-07-22 18:28:21 +0000

    Fixed to work with how writeMessage.js structures data

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -3,13 +3,26 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on("value", (snapshot) => {
         const data = snapshot.val()
-        key = passcode.value
-        console.log(data[key])
-        message = document.querySelector("#message")
-        if (data[key] ==undefined ) {
-            alert("undefined")
+        for (let key in data){
+            console.log(data[key].passcode)
+            if ( passcode.value === data[key].passcode ) {
+                message.innerHTML =  data[key].message
+                break
+            }
+        error_message = document.querySelector("#error_message")
+        error_message.classList.remove("hidden")
+        error_message.innerHTML = "No such passcode"
+        message.innerHTML = ""
+
         }
-        message.innerHTML = data[key]
+        //key = passcode.value
+        //console.log(data[key])
+        //message = document.querySelector("#message")
+        // if (data[key] ==undefined ) {
+        //     message = document.querySelector("#error_message")
+        //     message.classList.remove("hidden")
+        //     message.innerHTML = "No such passcode"
+        // }
 
     } )
 }
