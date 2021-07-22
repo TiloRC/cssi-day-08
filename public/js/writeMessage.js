e39a0fd5a160453dcae97b822a4f9a4548d81c7a
TiloRC <tiloreneau@gmail.com>
2021-07-22 17:52:37 +0000

    button reads and logs passcode and message

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -1,6 +1,7 @@
 const onSubmit = () => {
     console.log("submit botton clicked");
-    const passcodeInput = document.querySelector("#passcode").value
-    console.log(passcodeInput)
+    const passcodeInput = document.querySelector("#passcode").value;
+    const messageInput = document.querySelector("#message").value;
+    console.log(passcodeInput, messageInput);
 
 }
